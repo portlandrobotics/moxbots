ae9c4864e378a99ae34d640369ee9f48a9aeebd3
Jeff Miller <git@git.com>
2017-09-02 10:35:30 -0700

    add comments.  add support for wheel encoders.

--- Robot_Blynk/Robot_Blynk.ino
+++ Robot_Blynk/Robot_Blynk.ino
@@ -12,6 +12,10 @@
 
 #include "CurieIMU.h"
 
+void setMotor(int id, int direction, int speed=-1);
+
+
+
 // Create the motor shield object with the default I2C address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 
@@ -34,19 +38,26 @@ void setup() {
   Serial.begin(115200);
   delay(1000);
 
-  Serial.println("Adafruit Motorshield v2 - DC Motor");
+  Serial.println("MoxBot booting");
 
+  // Set up pin connected to wheel encoder switch
   pinMode(7,INPUT_PULLUP);
 
+
+  // Set up motor driver boards
   AFMS.begin();  // create with the default frequency 1.6KHz
   //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
 
+  // assign the motor channels into the array
   for(int i=0;i<4;i++) {
     motors[i] = AFMS.getMotor(i+1);
     motors[i]->setSpeed(255);
   }
 
+  // start up the IMU, do some Gyro setup
   CurieIMU.begin();
+
+  // 250 degrees per second
   CurieIMU.setGyroRange(250);
 }
 
@@ -54,9 +65,9 @@ void setup() {
 
 //######### Subroutines ################################
 
-void setMotor(int id, int direction, int speed=-1);
+// Convenience function to manipulate the mototrs.  Mostly useful becuase two of the motors (2 and 4) are backwards
 
-void setMotor(int id, int direction, int speed)
+void setMotor(int id, int direction, int speed /* =-1*/)
 {
   int swap = direction;
   if((id == 2) || (id==4))
@@ -74,19 +85,23 @@ void setMotor(int id, int direction, int speed)
   
 }
 
+// convenience function to set motor speed
 void setMotorSpeed(int id, int speed)
 {
   motors[id-1]->setSpeed(speed);
 }
 
+// convert gyro reading to degrees
 float scaleGyro(int reading, int range) {
   return ( reading/32768.9)*range; 
 }
 
+// convert accelerometer reading to G's
 float scaleAccel(int reading, int range) {
   return (float)(((float)reading/(float)32768.0)*(float)range);
 }
 
+// read all 3 gyro axis and store their results, in degrees, to the referenced arguments
 void readGyroScaled(float &x, float &y, float &z) {
   int range = CurieIMU.getGyroRange();
   int rx,ry,rz;
@@ -95,6 +110,8 @@ void readGyroScaled(float &x, float &y, float &z) {
   y=scaleGyro(ry,range);
   z=scaleGyro(rz,range);
 }
+
+// read all 3 acceleromoter axis and store their results, in G's, to the referenced arguments
 void readAccelScaled(float &x, float &y, float &z) {
   int range = CurieIMU.getAccelerometerRange();
   int rx,ry,rz;
@@ -107,11 +124,9 @@ void readAccelScaled(float &x, float &y, float &z) {
 //########## LOOP ######################################
 void loop() {
 
-  //int i = digitalRead(7);
-  //Serial.println(i);
-  //return;
-
   #if 0
+  // this is a bunch of code we used to test the IMU sensor.  Left in here for future reference
+  
   CurieIMU.setAccelerometerRange(4);
   CurieIMU.autoCalibrateAccelerometerOffset(X_AXIS, 0);
   CurieIMU.autoCalibrateAccelerometerOffset(Y_AXIS, 0);
@@ -150,6 +165,7 @@ void loop() {
     
     
   }
+  // end disabled test code
 #endif
 
 
@@ -163,83 +179,93 @@ void loop() {
 
   delay(3000);
 
+  // calibrate the Gryo
+   CurieIMU.autoCalibrateGyroOffset();
+
+
+  // calibrate the accelerate
    CurieIMU.setAccelerometerRate(800);
 
-   CurieIMU.autoCalibrateGyroOffset();
+  // 2G range
   CurieIMU.setAccelerometerRange(2);
   CurieIMU.autoCalibrateAccelerometerOffset(X_AXIS, 0);
   CurieIMU.autoCalibrateAccelerometerOffset(Y_AXIS, 0);
   CurieIMU.autoCalibrateAccelerometerOffset(Z_AXIS, 1);
 
-#if 0
-  long n = 0;
-  for(int i=0;i<1000;i++) {
-    n += CurieIMU.getAccelerometerOffset(X_AXIS);
-    delay(2);
-  }
-  n /= 1000;
-  {
-    char buffer[512];
-    sprintf(buffer,"CAL %f",n);
-    Serial.println(buffer);
-  }
-  #endif
-  // done calibrating
-
-  delay(1000);
   int range = CurieIMU.getAccelerometerRange();
   int rx,ry,rz;
-  
+
+  // not sure if this helps, but take a last accelerometer measurement
+  //   and then subtract it from future measurements
   CurieIMU.readAccelerometer(rx,ry,rz);
   int xtracal=rx;
   
   Serial.println(xtracal);
 
   unsigned long startTime = micros();
+
+  // desired travel distance, presently in wheel clicks
+  float travel = 16*4;
+
+  // *** loop state variables ***
   float lastm = startTime;
-  float travel = 16;
   float velocity =0;
   float location = 0;
-  int countdown = 1000;
+
+  float lastSwitchEvent = 0;
+
+  float turnaccum = 0.;
+
+  // iteration counter for occasionally printing status messages
   int cup=0;
+
+  int lastread=2;
+  int holdoff = 0;
+
+  int countdown=10;
+  
   while(1) {
-    float x;//,y,z;
-    //readAccelScaled(x,y,z);
     CurieIMU.readAccelerometer(rx,ry,rz);
-    x=scaleAccel(rx-xtracal,range);
-    if (fabs(x) < .01)
-      x = 0;
-    
+    float x=scaleAccel(rx-xtracal,range);
+
+    // optional deadzone
+    //if (fabs(x) < .01)
+    //  x = 0;
+
+    // measure time since last loop interation
     float m = micros();
     float dt = (m-lastm)/1e6;
 
     velocity += 9.8*x*dt;
 
     //location += velocity * dt;
-    static int lastread=0;
-    static int holdoff = 0;
     int switchread = digitalRead(7);
-    if(holdoff > 0) {
-      holdoff--;
+    /*if(m-lastSwitchEvent < 10) {
+      // debounce
     }
-    else if(lastread != switchread) {
-      if(!lastread && switchread)
+    else*/
+    if(lastread != switchread) {
+      //if(!lastread && switchread)
         location++;
       lastread=switchread;
-      holdoff=500;
+      lastSwitchEvent = m;
+      Serial.print(switchread);
+      Serial.print(" ");
+      Serial.println(location);
     }
 
     if(cup++ % 10000 < 1) {
       float t = (m-startTime)/1e6;
-    char buffer[512];
-    
-    sprintf(buffer, "%d %f %f : %f %f : %f",rx,x,dt,velocity,location,velocity / t);
-    Serial.println(buffer);
+      char buffer[512];
+      
+      sprintf(buffer, "%d %f %f : %f %f : %f",rx,x,dt,velocity,location,velocity / t);
+      Serial.println(buffer);
     }
 
-    if(fabs(location) > travel)
+    if(fabs(location) > travel+.5)
       break;
 
+#if 0
     if (countdown > 0) {
       countdown--;
     }
@@ -249,33 +275,51 @@ void loop() {
          sprintf(buffer, "FAIL %f %f : %f %f",x,dt,velocity,location);
           Serial.println(buffer);
           return;
-    }
-          setMotor(MOTOR_FL,BACKWARD, 64);
-          setMotor(MOTOR_RL,BACKWARD, 64);
-          setMotor(MOTOR_FR,BACKWARD, 64);
-        setMotor(MOTOR_RR,BACKWARD, 64);
-        countdown = -1;
+      }
+#endif
+
+    float gx,gy,gz;
+    readGyroScaled(gx,gy,gz);
+
+    turnaccum += gz*dt;
+
+      
+      int motorspeedLeft = 0x80;
+      int motorspeedRight = 0x80 + turnaccum;
+
+              char buffer[512];
+         sprintf(buffer, "FAIL %f %f : %d %f",gz,turnaccum,motorspeedRight,location);
+          Serial.println(buffer);
+
+      setMotor(MOTOR_FL,BACKWARD, motorspeedLeft);
+      setMotor(MOTOR_RL,BACKWARD, motorspeedLeft);
+      setMotor(MOTOR_FR,BACKWARD, motorspeedRight);
+      setMotor(MOTOR_RR,BACKWARD, motorspeedRight);
+      countdown = -1;
+
+      #if 0
     }
     else {
       //countdown --;
       //if(countdown < -10000)
       //  break;
     }
-
+#endif
     lastm=m;
   }
 
+  // turn off the motors
   setMotor(MOTOR_FL,RELEASE);
   setMotor(MOTOR_RL,RELEASE);
   setMotor(MOTOR_FR,RELEASE);
   setMotor(MOTOR_RR,RELEASE);
 
-  delay(1000);
-
 
   return;
 
-
+#if 0
+  // this the gyro based turn code
+  //   we'll put this stuff back in soon
   
   setMotor(MOTOR_FL,FORWARD, 128);
   setMotor(MOTOR_RL,FORWARD, 128);
@@ -329,6 +373,7 @@ void loop() {
 
 
   return;
+#endif
 }
 
 
